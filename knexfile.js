63daa4f5cad1b036e58f1391426b0120fdd162da
Maria Trujillo <mptruj015@gmail.com>
2020-05-22 17:27:31 +0200

    initialized project with needed deps, configured knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,30 @@
+//if doing heroku pg import here
+
 module.exports = {
   development: {
     client: 'sqlite3',
-    connection: { filename: './database/auth.db3' },
+    connection: { 
+      filename: './database/auth.db3' 
+    },
     useNullAsDefault: true,
     migrations: {
       directory: './database/migrations',
       tableName: 'dbmigrations',
     },
-    seeds: { directory: './database/seeds' },
+    seeds: { 
+      directory: './database/seeds' 
+    },
   },
+  testing: {
+    useNullAsDefault: true,
+    connection: {
+      filename: ":memory:",
+    },
+    migrations: {
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    }
+  }
 };
