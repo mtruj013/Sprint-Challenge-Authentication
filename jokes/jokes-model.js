c30281cd3e2e815c28207e9f4193c9f47e5d5cd3
Maria Trujillo <mptruj015@gmail.com>
2020-05-22 17:40:52 +0200

    set up jokes model

---
+++ jokes/jokes-model.js
@@ -0,0 +1,22 @@
+const db = require("../database/dbConfig.js");
+
+module.exports = {
+    find, 
+    add,
+    findByUsername,
+}
+
+function find(){
+    return db('users');
+}
+
+function add(user){
+    return db('users')
+    .insert(user, 'id')
+    // .then(([id]) => id)
+}
+
+function findByUsername(username) {
+    return db('users')
+    .where({ username }).first();
+}
