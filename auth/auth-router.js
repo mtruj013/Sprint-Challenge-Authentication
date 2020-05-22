f6543c17baa3a71c4f5208bd501383520d800d3b
Maria Trujillo <mptruj015@gmail.com>
2020-05-22 17:58:10 +0200

    added middleware

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,4 +1,8 @@
 const router = require('express').Router();
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const Users = require('../jokes/jokes-model.js')
 
 router.post('/register', (req, res) => {
   // implement registration
